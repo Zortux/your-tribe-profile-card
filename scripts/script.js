7b704298b15bc6ed59b93e4bbe71a27cc737e2b2
MohammedDahman <2131690@talnet.nl>
2023-09-13 10:45:03 +0200

    tt

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') // Een variabele en de query selecteren
+expandButton.addEventListener('click', expand)// Als je op de button klikt dan gebeurt er iets.
 
 function expand () {
   document.body.classList.toggle('expand')
-}
+}// De functie voor expand, hier roept hij de functie aan.
